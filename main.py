73cdcebf3ca636b764f36dad0e28fd42952c5699
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-07-06 20:28:55 -0300

    add post animal and change atributes to english

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 animals = [
-    {"id": 1, "nome": "Nina", "tipo": "cachorro"},
-    {"id": 2, "nome": "Preta", "tipo": "cachorro"},
-    {"id": 3, "nome": "Pity", "tipo": "cachorro"}
+    {"id": 1, "name": "Nina", "type": "cachorro"},
+    {"id": 2, "name": "Preta", "type": "cachorro"},
+    {"id": 3, "name": "Pity", "type": "cachorro"}
 ]
 
 #Get all
@@ -21,6 +21,17 @@ def get_animal_by_id(animal_id):
         return jsonify(animal)
     return jsonify({"messagem":"Animal não encontrado"}), 404
 
+
+@app.route('/animals', methods=['POST'])
+def save_animal():
+    new_animal = {
+        "id": len(animals) + 1,
+        "name": request.json['name'],
+        "type": request.json['type']
+    }
+    
+    animals.append(new_animal)
+    return jsonify(new_animal), 201
 
 #server execution
 if __name__ == '__main__':
