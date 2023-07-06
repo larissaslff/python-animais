bfc5e14d3da71b62331828ac801e75851c8b8896
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-07-06 20:08:56 -0300

    add get animals

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, world'
+animals = [
+    {"id": 1, "nome": "Nina", "tipo": "cachorro"},
+    {"id": 2, "nome": "Preta", "tipo": "cachorro"},
+    {"id": 3, "nome": "Pity", "tipo": "cachorro"}
+]
 
+#Get all
+@app.route('/animals', methods=['GET'])
+def all_animals():
+    return jsonify(animals)
+
+
+#server execution
 if __name__ == '__main__':
     app.run()
